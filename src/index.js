984bcb2b6819237c22f69b6410bdcaa13dfc42ff
Oleksandr Honcharov <o.v.goncharov90@gmail.com>
2022-12-15 12:08:19 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -4,7 +4,6 @@ import { Provider } from 'react-redux';
 import { App } from './components/App';
 import { store } from './redux/store';
 import './index.css';
-// import { PersistGate } from 'redux-persist/integration/react';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
