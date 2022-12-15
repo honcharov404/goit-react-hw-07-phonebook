1636b3fdb7587479227474a7298aef226da5ab32
Oleksandr Honcharov <o.v.goncharov90@gmail.com>
2022-12-15 12:16:03 +0200

    Update store1.js

--- src/redux/store1.js
+++ src/redux/store1.js
@@ -1,6 +1,7 @@
 import { configureStore } from '@reduxjs/toolkit';
-
 import { contactsReducer } from './contactsSlice/contactsSlice';
+
+// import { contactsReducer } from './contactsSlice/contactsSlice';
 import { filterReducer } from './filterSlice/filterSlice';
 
 export const store = configureStore({
