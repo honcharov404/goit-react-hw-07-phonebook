79205f775c045117c75bac042b8e9d41f00ab8fc
Oleksandr Honcharov <o.v.goncharov90@gmail.com>
2022-12-15 12:18:19 +0200

    1

--- src/redux/store1.js
+++ src/redux/store1.js
@@ -1,5 +1,6 @@
 import { configureStore } from '@reduxjs/toolkit';
-import { contactsReducer } from './contactsSlice/contactsSlice';
+import { contactsReducer } from './contactsSlice/ContactsSlice';
+// import { contactsReducer } from './contactsSlice/contactsSlice2';
 
 // import { contactsReducer } from './contactsSlice/contactsSlice';
 import { filterReducer } from './filterSlice/filterSlice';
